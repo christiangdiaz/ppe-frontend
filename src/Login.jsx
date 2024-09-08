9f87f9ffb388506c61d35ed477bd7c097e7be5e4
cdiaz2024 <cgd6055@gmail.com>
2024-09-08 15:11:25 -0400

    Added view password toggle

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,12 +4,27 @@ import mainPicture from './images/PPE.jpg';
 import { useNavigate } from 'react-router-dom';
 import { db } from './firebase'; // Import the Firestore instance
 import { doc, updateDoc } from 'firebase/firestore'; // Import Firestore methods
+import { Icon } from 'react-icons-kit';
+import { eyeOff } from 'react-icons-kit/feather/eyeOff';
+import { eye } from 'react-icons-kit/feather/eye';
 
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [type, setType] = useState('password');
+  const [icon, setIcon] = useState(eye);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+  
+  const handleToggle = () => {
+    if (type === 'password') {
+      setIcon(eyeOff);
+      setType('text');
+    } else {
+      setIcon(eye);
+      setType('password');
+    }
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -22,9 +37,8 @@ const Login = ({ onLogin }) => {
       });
 
       if (response.status === 200) {
-        onLogin(response.data.token); // Pass the custom token to the parent component
+        onLogin(response.data.token);
 
-        // Update last login time in Firestore
         const userDoc = doc(db, 'users', username);
         await updateDoc(userDoc, {
           lastLogin: new Date()
@@ -56,16 +70,22 @@ const Login = ({ onLogin }) => {
               className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition duration-200 ease-in-out"
             />
           </div>
-          <div>
+          <div className="relative">
             <label className="block text-gray-700 mb-2" htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition duration-200 ease-in-out"
-            />
+              <input
+                type={type}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition duration-200 ease-in-out"
+              />
+            <span 
+              className="absolute right-3 bottom-1 transform -translate-y-2/4 cursor-pointer flex items-center"
+              onClick={handleToggle}
+            >
+              <Icon icon={icon} size={25} />
+            </span>
           </div>
           <button
             type="submit"
